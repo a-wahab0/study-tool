aa0ef3c03cebe182ea464d4c1656e9f7ac72107d
Abdul Wahab <zalimclown@gmail.com>
2026-09-06 14:14:08 +0500

    Add bytesToBlob function to convert Uint8Array to Blob

--- core-lib/utils.ts
+++ core-lib/utils.ts
@@ -21,3 +21,7 @@ export function downloadBlob(blob: Blob, filename: string): void {
   // Release memory once the download has been triggered.
   setTimeout(() => URL.revokeObjectURL(url), 2000);
 }
+export function bytesToBlob(bytes: Uint8Array, type: string): Blob {
+  const copy = new Uint8Array(bytes);
+  return new Blob([copy.buffer as ArrayBuffer], { type });
+}
